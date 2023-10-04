f15cf16fdbd1a7472b81bcd74abc92348bf73461
Flávio Farias <fs.flaviosilv4@gmail.com>
2023-10-04 09:23:43 -0300

    Projeto inicializado no Spring

---
+++ Project/src/main/java/com/generation/mercadela/MercadelaApplication.java
@@ -0,0 +1,13 @@
+package com.generation.mercadela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadelaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadelaApplication.class, args);
+	}
+
+}
---
+++ Project/src/test/java/com/generation/mercadela/MercadelaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.mercadela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadelaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
