fe685583bc50813bcd1e642139ab1983d7e24af0
Flávio Farias <fs.flaviosilv4@gmail.com>
2023-10-06 12:08:18 -0300

    Commit controler funcional

---
+++ Project/src/main/java/com/generation/mercadela/controller/CategoriaController.java
@@ -0,0 +1,63 @@
+package com.generation.mercadela.controller;
+
+import com.generation.mercadela.model.Categoria;
+import com.generation.mercadela.repository.CategoriaRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaController {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Categoria>> getAll() {
+        return ResponseEntity.ok(categoriaRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Categoria> getById(@PathVariable Long id) {
+        return categoriaRepository.findById(id)
+                .map(resposta -> ResponseEntity.ok(resposta))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    @GetMapping("/nomeCategoria/{nomeCategoria}")
+    public ResponseEntity<List<Categoria>> getBynomeCategoria(@PathVariable String nomeCategoria){
+        return ResponseEntity.ok(categoriaRepository.findAllByNomeCategoriaContainingIgnoreCase(nomeCategoria));
+    }
+
+    @PutMapping
+    public ResponseEntity<Categoria> put(@Valid @RequestBody Categoria categoria){
+        return categoriaRepository.findById(categoria.getId())
+                .map(resposta -> ResponseEntity.status(HttpStatus.OK)
+                        .body(categoriaRepository.save(categoria)))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Categoria> post(@Valid @RequestBody Categoria categoria){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(categoriaRepository.save(categoria));
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        Optional<Categoria> categoria = categoriaRepository.findById(id);
+
+        if(categoria.isEmpty())
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        categoriaRepository.deleteById(id);
+    }
+}
