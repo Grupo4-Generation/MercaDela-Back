97f3b557d7746b66af94b2ca30f910dcd1449604
Matheus Pereira da Silva <matheus.s280604@gmail.com>
2023-10-06 10:30:25 -0300

    Update Categoria.java

--- Project/src/main/java/com/generation/mercadela/model/Categoria.java
+++ Project/src/main/java/com/generation/mercadela/model/Categoria.java
@@ -1,14 +1,6 @@
 package com.generation.mercadela.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import java.util.List;
