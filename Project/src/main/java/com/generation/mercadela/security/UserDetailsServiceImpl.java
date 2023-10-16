e4da8e37077e2356d82cabe27f3a1ac481faccd6
Julia Domingues <142114663+julooch@users.noreply.github.com>
2023-10-16 11:11:20 -0300

    Desenvolvimento do pacote Security

---
+++ Project/src/main/java/com/generation/mercadela/security/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.generation.mercadela.security;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.generation.mercadela.model.Usuario;
+import com.generation.mercadela.repository.UsuarioRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@Autowired
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+		
+		Optional<Usuario> usuario = usuarioRepository.findByEmailUsuario(userName);
+		
+		if (usuario.isPresent())
+			return new UserDetailsImpl(usuario.get());
+		else
+			throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+	}
+}
