1936e64bddfa172b112413f3b439b09ff05efe3d
Flávio Farias <fs.flaviosilv4@gmail.com>
2023-11-17 13:19:21 -0300

    CORS foi pra casa branca fodas

--- src/main/java/com/generation/mercadela/controller/CategoriaController.java
+++ src/main/java/com/generation/mercadela/controller/CategoriaController.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/categoria")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class CategoriaController {
 
     @Autowired
